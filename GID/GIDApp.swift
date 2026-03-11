44321c537f37846015563ae5202af84d03800894
Gabriel Aparicio <195454686+GabeAparicio@users.noreply.github.com>
2026-03-11 18:30:16 -0400

    Initial Commit

---
+++ GID/GIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIDApp.swift
+//  GID
+//
+//  Created by Gabriel Aparicio on 2026-03-11.
+//
+
+import SwiftUI
+
+@main
+struct GIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
