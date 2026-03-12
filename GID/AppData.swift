daf73b39f39347522539f8b102e172c8fcd0bb23
Gabriel Aparicio <195454686+GabeAparicio@users.noreply.github.com>
2026-03-11 20:31:32 -0400

    Added AppData.swift file

--- GID/AppData.swift
+++ GID/AppData.swift
@@ -1,7 +1,14 @@
-//
-//  AddData.swift
-//  GID
-//
-//  Created by Gabriel Aparicio on 2026-03-11.
-//
+import Foundation
+import Combine
 
+final class AppData: ObservableObject {
+    
+    @Published var tasks: [Task] = [
+        Task(title: "Gym Workout", description: "Leg day and abs, 15 mins cardio warm up", dueDate: "Today 5PM", priority: "Medium"),
+        Task(title: "Buy Groceries", description: "Milk, eggs, fruit, bread", dueDate: "Today 8PM", priority: "High"),
+        Task(title: "Quiz", description: "Study SwiftUI notes", dueDate: "Friday", priority: "High"),
+        Task(title: "Clean Room", description: "Vacuum and organize desk", dueDate: "Tomorrow", priority: "Low")
+    ]
+    
+    @Published var selectedTask: Task? = nil
+}
