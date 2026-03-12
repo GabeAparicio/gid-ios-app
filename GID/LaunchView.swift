23619da1f9b5f6bc3c8d3e6714d10065302369b6
Wassn23 <wassn.hashim@gmail.com>
2026-03-11 21:07:51 -0400

    launch screen with logo

---
+++ GID/LaunchView.swift
@@ -0,0 +1,48 @@
+import SwiftUI
+
+struct LaunchView: View {
+    
+    var body: some View {
+        
+        ZStack {
+            
+            LinearGradient(
+                colors: [
+                    Color(hex: "#B5E8FF").opacity(0.55),
+                    Color(hex: "#6D8B99").opacity(0.65)
+                ],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+            
+            VStack {
+                
+                Spacer()
+                
+                Text("GID!")
+                    .font(.system(size: 72, weight: .black, design: .rounded))
+                    .foregroundColor(.white)
+                
+                Text("GET IT DONE!")
+                    .font(.system(size: 22, weight: .bold))
+                    .foregroundColor(.white)
+                    .padding(.top, 8)
+                
+                Spacer()
+                
+                VStack(spacing: 4) {
+                    Text("Gabriel Aparicio")
+                    Text("Wassn Al Nabhan")
+                }
+                .font(.system(size: 16, weight: .medium))
+                .foregroundColor(.white)
+                .padding(.bottom, 36)
+            }
+        }
+    }
+}
+
+#Preview {
+    LaunchView()
+}
